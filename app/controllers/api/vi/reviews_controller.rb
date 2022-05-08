913ab78bcefe0e641aece90f081507c2b4efab51
min <minwang.bus@gmail.com>
2022-05-08 19:10:01 -0400

    added model route controller

---
+++ app/controllers/api/vi/reviews_controller.rb
@@ -0,0 +1,5 @@
+module Api
+    class V1::ReviewsController < ApplicationController
+    
+    end
+end
---
+++ app/serializers/review_serializer.rb
@@ -0,0 +1,6 @@
+class ReviewSerializer
+  include JSONAPI::Serializer
+  attributes :title, :description, :score, :airline_id
+
+  
+end
