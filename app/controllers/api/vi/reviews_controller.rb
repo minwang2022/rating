86180297f01af6d3fe95d27a9adb7b19100665db
min <minwang.bus@gmail.com>
2022-05-09 17:00:38 -0400

    reviews controller

--- app/controllers/api/vi/reviews_controller.rb
+++ app/controllers/api/vi/reviews_controller.rb
@@ -1,5 +1,31 @@
 module Api
     class V1::ReviewsController < ApplicationController
-    
+        
+        def create
+            review = Review.new(review_param)
+
+            if review.save
+                render json: ReviewSerializer.new(review).serialized_json
+            else 
+                render json: {error: review.errors.messages}, status: 422
+            end
+        end
+
+        def create
+            review = Review.find_by(params[:id])
+
+            if review.destroy
+                head :no_content 
+                
+            else 
+                render json: {error: review.errors.messages}, status: 422
+            end
+        end
+
+
+        private 
+        def review_params
+            params.require(:review).permit(:title, :description, :score, :airline_id)
+        end
     end
 end
